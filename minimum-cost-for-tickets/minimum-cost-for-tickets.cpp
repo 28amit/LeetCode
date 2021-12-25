4e484be1ada1909ff5b2b44cbb6d3f2d207e879d
28amit <amit28amical@gmail.com>
2021-12-25 15:24:45 +0530

    Time: 8 ms (20.61%), Space: 10.1 MB (18.28%) - LeetHub

--- minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-    int n;
-    unordered_set<int>s;
-    int solve(vector<int>& days, vector<int>& costs,int cday,vector<int>& dp){
-        if(cday>days[n-1])
-            return dp[cday]=0;
-        if(dp[cday]!=-1)
-            return dp[cday];
-        int ans=INT_MAX;
-        if(s.count(cday)){
-            ans=solve(days,costs,cday+1,dp)+costs[0];
-            ans=min(ans,solve(days,costs,cday+7,dp)+costs[1]);
-            ans=min(ans,solve(days,costs,cday+30,dp)+costs[2]);
-        }
-        else
-            return dp[cday]=solve(days,costs,cday+1,dp);
-        return dp[cday]=ans;
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        n=days.size();
-        vector<int>dp(days[n-1]+31,-1);
-        for(int i=0;i<n;i++){
-            s.insert(days[i]);
+        vector<int>dp(400);
+        unordered_set s(days.begin(),days.end());
+        for(int i=1;i<366;i++){
+            dp[i]=dp[i-1];
+            if(s.count(i)){
+                dp[i]=min({dp[i-1]+costs[0],dp[max(0,i-7)]+costs[1],dp[max(0,i-30)]+costs[2]});
+            }
         }
-        return solve(days,costs,0,dp);
+        return dp[365];
     }
 };
