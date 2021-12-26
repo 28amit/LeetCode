c8e35559a302684e4eab83f4ccc50c203ccaa44d
28amit <amit28amical@gmail.com>
2021-12-26 19:21:21 +0530

    Time: 256 ms (21.78%), Space: 12 MB (41.00%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -9,19 +9,22 @@ public:
             return 0;
         vector<vector<bool>>dp(n+1,vector<bool>(sum/2+1));
         for(int i=0;i<=n;i++){
-            dp[i][0]=true;
-        }
-        for(int i=1;i<=sum/2;i++){
-            dp[0][i]=false;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum/2;j++){
-                if(nums[i-1]>j)
-                    dp[i][j]=dp[i-1][j];
-                else
-                    dp[i][j]=dp[i-1][j]||dp[i-1][j-nums[i-1]];
+            for(int j=0;j<=sum/2;j++){
+                if(i==0&j==0)
+                    dp[i][j]=true;
+                else if(i==0){
+                    dp[i][j]=false;
+                }
+                else if(j==0)
+                    dp[i][j]=true;
+                else{
+                    if(nums[i-1]>j)
+                        dp[i][j]=dp[i-1][j];
+                    else
+                        dp[i][j]=dp[i-1][j]||dp[i-1][j-nums[i-1]];
                 }   
             }
+        }
         return dp[n][sum/2];
     }
 };
