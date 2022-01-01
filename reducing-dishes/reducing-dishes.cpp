8530f31ed38fefabe66117d6093caf4d8c6fbe52
28amit <amit28amical@gmail.com>
2022-01-01 19:08:36 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (61.34%) - LeetHub

--- reducing-dishes/reducing-dishes.cpp
+++ reducing-dishes/reducing-dishes.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> memo;
-int dp(int i,int t,vector<int>& arr)
-{
-    if(i>=arr.size())
-        return 0;
-    if(memo[i][t]!=-1)
-        return memo[i][t];
-    int not_taken=dp(i+1,t,arr);
-    int taken=arr[i]*t+dp(i+1,t+1,arr);
-    return memo[i][t]=max(taken,not_taken);
-}
-int maxSatisfaction(vector<int>& arr) 
-{
-    sort(arr.begin(),arr.end());
-    int n=arr.size();
-    memo.resize(n,vector<int>(n+1,-1));
-    return dp(0,1,arr);
-}
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(), satisfaction.end());
+        int res = 0, total = 0, n = satisfaction.size();
+        for (int i = n - 1; i >= 0 && satisfaction[i] > -total; --i) {
+            total += satisfaction[i];
+            res += total;
+        }
+        return res;
+    }
 };
