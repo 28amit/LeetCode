2a12e2e10858b9159b61a29b691838449a81eb4d
28amit <amit28amical@gmail.com>
2022-01-10 19:05:15 +0530

    Time: 184 ms (14.16%), Space: 67.6 MB (91.59%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans=nums[0];
+        int tmp=0;
+        for(int i=0;i<nums.size();i++){
+            tmp+=nums[i];
+            if(tmp>ans){
+                ans=tmp;
+            }
+            if(tmp<0){
+                tmp=0;
+            }
+        }
+        return ans;
+    }
+};
