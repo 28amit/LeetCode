dadb97050613aee9614a76a68cb2ebda5b9ebb85
28amit <amit28amical@gmail.com>
2022-02-02 17:14:55 +0530

    Time: 30 ms (51.77%), Space: 22.4 MB (8.86%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -4,6 +4,8 @@ public:
         int l=0,r=nums.size()-2;
         while(l<=r){
             int mid=l+(r-l)/2;
+            if(mid<nums.size()-1&&mid!=0&&nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1])
+                return nums[mid];
             if(nums[mid]==nums[mid^1])
                 l=mid+1;
             else
