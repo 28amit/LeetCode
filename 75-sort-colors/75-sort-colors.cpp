054550705dc8ddad5a560be00110f9a717e9b298
28amit <amit28amical@gmail.com>
2022-02-05 12:13:12 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (23.72%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int s=0,e=n-1,m=0;
+        while(m<=e){
+            if(nums[m]==2){
+                swap(nums[m],nums[e]);
+                e--;
+            }
+            else if(nums[m]==0){
+                swap(nums[m],nums[s]);
+                s++;
+                m++;
+            }
+            else{
+                m++;
+            }
+        }
+    }
+};
