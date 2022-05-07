4e5bac985d90a4718df02edc56b5dafc3a545021
28amit <amit28amical@gmail.com>
2022-05-07 23:01:43 +0530

    Time: 130 ms (65.81%), Space: 67.7 MB (55.12%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -5,12 +5,10 @@ public:
         int tmp=0;
         for(int i=0;i<nums.size();i++){
             tmp+=nums[i];
-            if(tmp>ans){
+            if(tmp>ans)
                 ans=tmp;
-            }
-            if(tmp<0){
+            if(tmp<0)
                 tmp=0;
-            }
         }
         return ans;
     }
